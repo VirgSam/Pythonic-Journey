85992039f412d6425fa23f9fb0795f1041b84b06
chris01 <chris_odum@yahoo.com>
2023-04-20 05:07:30 +0200

    done

---
+++ w3resource/EX02.py
@@ -0,0 +1,16 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#Exercise 2
+# function to return current version of python
+import sys
+def return_version():
+    x = sys.version
+    return x
+
+return_version()
+
+#Solution in book
+# import sys
+# print("Python version")
+# print (sys.version)
+# print("Version info.")
+# print (sys.version_info)
