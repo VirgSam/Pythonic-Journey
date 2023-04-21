722d5b8186acab2e3ab5d449528be23510760098
VirgSam <chris_odum@yahoo.com>
2023-04-21 07:04:24 +0200

    task 11 done

---
+++ w3resource/basic01/EX11.py
@@ -0,0 +1,23 @@
+# 11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+# Sample function : abs()
+# Expected Result :
+# abs(number) -> number
+# Return the absolute value of the argument
+
+print(abs.__doc__)
+#print(aiter.__doc__)
+print(all.__doc__)
+print(any.__doc__)
+#print(anext.__doc__)
+print(bin.__doc__)
+print(bool.__doc__)
+print(breakpoint.__doc__)
+print(bytearray.__doc__)
+print(bytes.__doc__)
+print(classmethod.__doc__)
+print(chr.__doc__)
+print(compile.__doc__)
+print(complex.__doc__)
+print(delattr.__doc__)
+print(dict.__doc__)
+print(dir.__doc__)
