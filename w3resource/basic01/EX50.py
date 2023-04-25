1d10980506317b05a25f9075af3b59f0ec658e93
chris01 <chris_odum@yahoo.com>
2023-04-25 06:46:15 +0200

    done

---
+++ w3resource/basic01/EX50.py
@@ -0,0 +1,11 @@
+# 50. Write a Python program to print without newline or space.
+
+for i in range(0, 10):
+    print('*', end="") # removing the end="" changed the print output.
+print("\n")
+
+
+# Notes
+# might be important to know which arguments do what within the print parentheses
+# also how to read white docs
+# https://www.youtube.com/watch?v=vYuvEWiffts : How to Read the Python Documentation (Tutorial)
