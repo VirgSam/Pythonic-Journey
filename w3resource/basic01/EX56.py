001345ac200f9d52d1e7623edefce3d88e3808dc
chris01 <chris_odum@yahoo.com>
2023-04-26 15:49:50 +0200

    done

---
+++ w3resource/basic01/EX56.py
@@ -0,0 +1,12 @@
+# 56. Write a Python program to get height and width of the console window.
+# import os
+# ts = os.get_terminal_size()
+# print(ts.columns)
+# print(ts.lines)
+
+# Alternative soln 1
+# import os
+# hw = os.get_terminal_size()
+# print("Height:", hw[1], "Width:", hw[0])
+
+# This will only run in Linux based enviroment for some reason.
