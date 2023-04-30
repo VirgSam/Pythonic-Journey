1446c9364d99a6d921ecd49ad12041afe7bf6e77
chris01 <chris_odum@yahoo.com>
2023-04-30 02:59:08 +0200

    done

---
+++ automate_python/loopIterateOverListIndex.py
@@ -0,0 +1,4 @@
+# this is a technique for iterating over a list using the range function
+supplies = ["pens", "staplers", "flamethrowers", "binders"]
+for i in range(len(supplies)):
+    print("index " + str(i) + " in supplies is: " + supplies[i])
