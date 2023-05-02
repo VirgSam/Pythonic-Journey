f32fb0bdb9ab2cda09280cf9c077dd971425b163
chris01 <chris_odum@yahoo.com>
2023-05-02 06:46:08 +0200

    done

---
+++ automate_python/tupleUnpacking.py
@@ -0,0 +1,10 @@
+cat = ["fat", "gray", "loud"]
+size = cat[0]
+color = cat[1]
+disposition = cat[2]
+
+# tuple unpacking is a shortcut that allows you to assing multiple varibles to a list
+# in 1 line of code
+
+cat = ["fat", "gray", "loud"]
+size, color, disposition = cat
