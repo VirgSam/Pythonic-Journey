cf623ca90d9b7326c88707b84b5fc42e16a6cb08
chris01 <chris_odum@yahoo.com>
2023-05-04 07:48:12 +0200

    done

--- udemy/max_sch/feedback/feedback/reviews/forms.py
+++ udemy/max_sch/feedback/feedback/reviews/forms.py
@@ -7,3 +7,6 @@ class ReviewFORM(forms.Form):
     "max_length":"Please enter a shorter name!",
 
     })# required true is always default, so no need to add it.
+
+    review =forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+    ratings =forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
