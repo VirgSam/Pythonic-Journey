1f9798e558638b59e6eef9bfd6b508bdee7af70d
chris01 <chris_odum@yahoo.com>
2023-05-04 07:24:04 +0200

    done

--- udemy/max_sch/feedback/feedback/reviews/forms.py
+++ udemy/max_sch/feedback/feedback/reviews/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class ReviewFORM(forms.Form):
-    user_name=forms.CharField(label="Your Name", max_length=100)
+    user_name=forms.CharField(label="Your Name",required=True, max_length=100, error_messages={
+                                            
+    "required":"Your Name Must Not Be Empty",
+    "max_length":"Please enter a shorter name!",
+
+    })# required true is always default, so no need to add it.
