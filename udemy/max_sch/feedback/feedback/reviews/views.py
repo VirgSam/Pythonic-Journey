ef4891ac140164df8897fc6c38842a3f6515d4b5
chris01 <chris_odum@yahoo.com>
2023-05-04 07:02:02 +0200

    done

--- udemy/max_sch/feedback/feedback/reviews/views.py
+++ udemy/max_sch/feedback/feedback/reviews/views.py
@@ -10,7 +10,8 @@ def review(request):
        if form.is_valid():
           print(form.cleaned_data)
           return HttpResponseRedirect("/thank-you")
-    form = ReviewFORM()
+    else:
+        form = ReviewFORM()
     return render(request,"reviews/review.html",{"form":form})
 
 def thank_you(request):
