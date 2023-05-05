47f9edfad42ec46d5687e9e946a762118c277913
chris01 <chris_odum@yahoo.com>
2023-05-05 08:41:26 +0200

    done

--- udemy/max_sch/feedback/feedback/reviews/views.py
+++ udemy/max_sch/feedback/feedback/reviews/views.py
@@ -10,27 +10,33 @@ class ReviewView(View):
    Views as classes
    """
    def get(self,request):
-      pass
+      form = ReviewFORM()
+      return render(request,"reviews/review.html",{"form":form})
+   
    def post(self,request):
-      pass
+      form = ReviewFORM(request.POST)
+      if form.is_valid():
+         form.save()
+         return HttpResponseRedirect("/thank-you")
+      return render(request,"reviews/review.html",{"form":form})
    
 
-def review(request):
-    """
-    Views as functions
-    """
-    if request.method == "POST":
-       form = ReviewFORM(request.POST)
-       if form.is_valid():
-        #   re_review = Review(user_name=form.cleaned_data['user_name'],
-        #                      review=form.cleaned_data['review'],
-        #                      ratings=form.cleaned_data['ratings'])
-        #  re_review.save() the old way
-        form.save() # saving with the modelform utilizing its greater functionality over Review(models.Model)
-        return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewFORM()
-    return render(request,"reviews/review.html",{"form":form})
+# def review(request):
+#     """
+#     Views as functions
+#     """
+#     if request.method == "POST":
+#        form = ReviewFORM(request.POST)
+#        if form.is_valid():
+#         #   re_review = Review(user_name=form.cleaned_data['user_name'],
+#         #                      review=form.cleaned_data['review'],
+#         #                      ratings=form.cleaned_data['ratings'])
+#         #  re_review.save() the old way
+#         form.save() # saving with the modelform utilizing its greater functionality over Review(models.Model)
+#         return HttpResponseRedirect("/thank-you")
+#     else:
+#         form = ReviewFORM()
+#     return render(request,"reviews/review.html",{"form":form})
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
