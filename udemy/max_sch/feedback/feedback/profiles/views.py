eaa5e7ca746f2897e9b1e2bced753b591510751a
chris01 <chris_odum@yahoo.com>
2023-05-08 08:44:04 +0200

    using the CreateView

--- udemy/max_sch/feedback/feedback/profiles/views.py
+++ udemy/max_sch/feedback/feedback/profiles/views.py
@@ -34,3 +34,5 @@ from django.views.generic.edit import CreateView
 class CreateProfileView(CreateView):
     template_name = "profiles/create_profile.html"
     model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
