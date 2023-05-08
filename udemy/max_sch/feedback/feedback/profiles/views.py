89488f3812ffc0bc487e7aa9ec6fb7b7a04be8df
chris01 <chris_odum@yahoo.com>
2023-05-08 08:41:35 +0200

    using the CreateView

--- udemy/max_sch/feedback/feedback/profiles/views.py
+++ udemy/max_sch/feedback/feedback/profiles/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from . forms import ProfileForm
 from . models import UserProfile
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 # made redundant as UserProfile model handles 
@@ -15,16 +16,21 @@ from . models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {"form": form})
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {"form": form})
+#     def post(self, request):
+#         submitted_form= ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+#         return render(request, "profiles/create_profile.html", {"form": submitted_form})
+    
 
-    def post(self, request):
-        submitted_form= ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        return render(request, "profiles/create_profile.html", {"form": submitted_form})
+# Using the CreateView as opposed to the View
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
