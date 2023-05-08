d229f4cfcd3891c60c020e5cad01717b1764e92b
chris01 <chris_odum@yahoo.com>
2023-05-08 08:19:31 +0200

    imagefield

--- udemy/max_sch/feedback/feedback/profiles/models.py
+++ udemy/max_sch/feedback/feedback/profiles/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    image = models.FileField( upload_to="image")
+    #image = models.FileField( upload_to="image") # using an FileField
+    image = models.ImageField( upload_to="image") # using an ImageField
