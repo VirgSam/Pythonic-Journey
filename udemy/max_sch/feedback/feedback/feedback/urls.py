fa0f6d9822a40739d2c7514c1d69f66f371a0e53
chris01 <chris_odum@yahoo.com>
2023-05-09 06:27:05 +0200

    django serving updated files

--- udemy/max_sch/feedback/feedback/feedback/urls.py
+++ udemy/max_sch/feedback/feedback/feedback/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("reviews.urls")),
     path('profiles/', include("profiles.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
