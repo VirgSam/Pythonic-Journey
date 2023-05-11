5ac4e5fbe9536236d1fccd24da35f990d8590afb
chris01 <chris_odum@yahoo.com>
2023-05-11 08:35:28 +0200

    Create SinglePostView url path

--- udemy/max_sch/my_site/my_site/blog/urls.py
+++ udemy/max_sch/my_site/my_site/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.StartingPageView.as_view(), name="starting-page"),
     path("posts",views.AllPostsView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>",views.posts_detail,name="posts-detail-page"),
+    path("posts/<slug:slug>",views.SinglePostView.as_view(),name="posts-detail-page"),
 
 ]
