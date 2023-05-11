46c85e0dd0c2d3423370822146e855a74d6ed7db
chris01 <chris_odum@yahoo.com>
2023-05-11 08:07:00 +0200

    Create StartingPageView class

--- udemy/max_sch/my_site/my_site/blog/views.py
+++ udemy/max_sch/my_site/my_site/blog/views.py
@@ -9,7 +9,8 @@ from . models import Post
 class StartingPageView(ListView):
     template_name = "blog/index.html"
     model = Post
-    ordering = ["-date"] 
+    ordering = ["-date"]
+    context_object_name = "posts" 
 
     def get_queryset(self):
         queryset= super().get_queryset()
