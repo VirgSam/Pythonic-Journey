af2877e79c9ff7f955754f9d77031d48cece671b
chris01 <chris_odum@yahoo.com>
2023-05-11 08:26:37 +0200

    Create AllPostsView class

--- udemy/max_sch/my_site/my_site/blog/urls.py
+++ udemy/max_sch/my_site/my_site/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.StartingPageView.as_view(), name="starting-page"),
-    path("posts",views.posts, name="posts-page"),
+    path("posts",views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>",views.posts_detail,name="posts-detail-page"),
 
 ]
