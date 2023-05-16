5344ae0b4fc640d9f206a1afefa6227fa2144120
chris01 <chris_odum@yahoo.com>
2023-05-16 11:15:50 +0200

    Refactor views.py SinglePostView class get and post method context dict

--- udemy/max_sch/my_site/my_site/blog/views.py
+++ udemy/max_sch/my_site/my_site/blog/views.py
@@ -38,17 +38,26 @@ class AllPostsView(ListView):
 #     return render (request,"blog/all-posts.html",{"all_posts":posts_list})
 
 class SinglePostView(View):
+    def is_stored_post(self,request,post_id):
+        stored_posts= request.session.get("stored_post")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        
+        return is_saved_for_later
     
     def get(self, request, slug):
         post=Post.objects.get(slug=slug)
-
         context= {
             "post": post,
             "post_tags":post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request,post.id)
         }
         return render(request,"blog/post-detail.html", context)
+        
         
 
     def post(self, request,slug):
@@ -64,7 +73,8 @@ class SinglePostView(View):
             "post": post,
             "post_tags":post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request,post.id)
         }
         return render(request,"blog/post-detail.html", context)
 
