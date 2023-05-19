7101f47d33ce1b63133397ac46e56643315b3be7
chris01 <chris_odum@yahoo.com>
2023-05-19 14:33:20 +0200

    Create a .env file with secret key

--- udemy/max_sch/my_site/my_site/my_site/settings.py
+++ udemy/max_sch/my_site/my_site/my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y%)584ca#v01ey0j4xho+=89ie(80=bv2zfo#@i^pl9sm4am%e'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
