3a94e3691a7696932e4c147b7209f76091250917
chris01 <chris_odum@yahoo.com>
2023-06-15 23:15:20 +0200

    Update home page index func

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups',views.index) #our_domain_name/meetups
+    path('meetups/',views.index) #our_domain_name/meetups
 ]
