0b698fcc033c95741a7761a6150b9c26878a6bd2
chris01 <chris_odum@yahoo.com>
2023-06-18 08:36:58 +0200

    Update meet_detail url path

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index), #our_domain_name/meetups
-    path('meetups/detail', views.meetup_detail), #our_domain_name/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_detail), #our_domain_name/meetups/dynamic path segment
 ]
