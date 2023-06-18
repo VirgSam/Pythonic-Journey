9dd599f958182ee65c0c4477fc1b0bc25ce31907
chris01 <chris_odum@yahoo.com>
2023-06-18 08:57:43 +0200

    Create named references for each path in urls.py

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index), #our_domain_name/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_detail), #our_domain_name/meetups/dynamic path segment
+    path('meetups/', views.index, name='all-meetups'), #our_domain_name/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'), #our_domain_name/meetups/dynamic path segment
 ]
