e119e1f501c43fe9568883865d7e90cc8d9d86b0
chris01 <chris_odum@yahoo.com>
2023-06-24 13:25:33 +0200

    Update Models.py create location class

--- udemy/max_sch/django_project_site/meetups/models.py
+++ udemy/max_sch/django_project_site/meetups/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    name= models.CharField(max_length=200)
+    address=models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.name} ({self.address})"
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
