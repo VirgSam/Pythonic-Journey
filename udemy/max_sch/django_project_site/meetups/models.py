b08435a2eb8432624d068cc4a5b73b468f3b8e87
chris01 <chris_odum@yahoo.com>
2023-06-25 11:57:18 +0200

    Update class Meetup with date and organiser_email fields

--- udemy/max_sch/django_project_site/meetups/models.py
+++ udemy/max_sch/django_project_site/meetups/models.py
@@ -16,11 +16,13 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organiser_email=models.EmailField(unique=True)
+    date=models.DateField()
     slug = models.SlugField(unique=True)
     description= models.TextField()
     image= models.ImageField(upload_to="images", height_field=None, width_field=None, max_length=None)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
-    participant=models.ManyToManyField(Participant, blank=True, null=True)
+    participants=models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
