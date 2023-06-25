d58b105199e708d00c1aa68acb4fa0a80845825c
chris01 <chris_odum@yahoo.com>
2023-06-25 11:50:48 +0200

    Create many to many relationship with class Participant and Meetup  in models.py

--- udemy/max_sch/django_project_site/meetups/models.py
+++ udemy/max_sch/django_project_site/meetups/models.py
@@ -20,7 +20,7 @@ class Meetup(models.Model):
     description= models.TextField()
     image= models.ImageField(upload_to="images", height_field=None, width_field=None, max_length=None)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
-    
+    participant=models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
