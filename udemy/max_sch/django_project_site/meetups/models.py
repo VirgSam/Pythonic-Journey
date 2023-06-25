a8d8405e1bbae697b3dbdb17a1fe60f85e42c512
chris01 <chris_odum@yahoo.com>
2023-06-25 11:47:10 +0200

    Create class Participant in models.py

--- udemy/max_sch/django_project_site/meetups/models.py
+++ udemy/max_sch/django_project_site/meetups/models.py
@@ -8,12 +8,19 @@ class Location(models.Model):
     def __str__(self):
         return f"{self.name} ({self.address})"
 
+class Participant(models.Model):
+    email=models.EmailField(unique=True)
+    def __str__(self):
+        return f"{self.email}"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description= models.TextField()
     image= models.ImageField(upload_to="images", height_field=None, width_field=None, max_length=None)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
+    
 
     def __str__(self):
         return f"{self.title}"
