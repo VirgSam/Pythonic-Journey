a395ee96a76637ba9126bd7d32feffcd390eae09
chris01 <chris_odum@yahoo.com>
2023-06-25 12:44:24 +0200

    Register class Participant on admin.py

--- udemy/max_sch/django_project_site/meetups/admin.py
+++ udemy/max_sch/django_project_site/meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
@@ -10,4 +10,5 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location )
+admin.site.register(Participant )
 
