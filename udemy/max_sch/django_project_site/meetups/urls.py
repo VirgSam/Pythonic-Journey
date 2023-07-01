071ebecd13aa0a7a8f9753ea65e5851b1bb2e2bb
chris01 <chris_odum@yahoo.com>
2023-07-01 21:02:26 +0200

    Update urls.py with confirm registration path

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'), #our_domain_name/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'),
-    path('meetups/confirm_registration', views.confirm_registration, name='confirm-registration'), #our_domain_name/meetups/dynamic path segment
+    path('meetups/confirm_registration', views.confirm_registration, name='confirm-registration'),
+    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'),#our_domain_name/meetups/dynamic path segment
+     
 ]
