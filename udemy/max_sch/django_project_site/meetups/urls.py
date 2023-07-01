bdb7c48f8e3ff2a8c6b75afa40ebd51c18b72502
chris01 <chris_odum@yahoo.com>
2023-07-01 15:34:00 +0200

    Update urls.py with new path to registration-success.html, create registration-success.html page

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'), #our_domain_name/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'), #our_domain_name/meetups/dynamic path segment
+    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'),
+    path('meetups/thank_you', views.meetup_detail, name='thank-you'), #our_domain_name/meetups/dynamic path segment
 ]
