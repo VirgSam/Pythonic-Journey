207b0ff608803a1098e88081b21c363117c3be43
chris01 <chris_odum@yahoo.com>
2023-07-02 17:03:32 +0200

    Update url.py path to avoid redundancy of using meetups/

--- udemy/max_sch/django_project_site/meetups/urls.py
+++ udemy/max_sch/django_project_site/meetups/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'), #our_domain_name/meetups
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
-    path('meetups/<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'),#our_domain_name/meetups/dynamic path segment
+    path('', views.index, name='all-meetups'), #our_domain_name/meetups
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+    path('<slug:meetup_slug>', views.meetup_detail, name='meetup-detail'),#our_domain_name/meetups/dynamic path segment
      
 ]
