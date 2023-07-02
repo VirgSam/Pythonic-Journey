9a4e695f4439e9c520f524fc27e073fedd8631fe
chris01 <chris_odum@yahoo.com>
2023-07-02 17:08:29 +0200

    Update url.py path to avoid redundancy of using meetups/

--- udemy/max_sch/django_project_site/django_project_site/urls.py
+++ udemy/max_sch/django_project_site/django_project_site/urls.py
@@ -22,6 +22,6 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ),
+    path('', RedirectView.as_view(url='meetups/') ), # research on this
     path('meetups/', include('meetups.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
