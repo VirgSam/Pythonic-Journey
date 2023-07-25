58671373c16e9ff5e1e52609522e41a1109aea7b
chris01 <chris_odum@yahoo.com>
2023-07-25 14:44:35 +0200

    Create folder system to manage Django-postgres request-response cycle

--- udemy/lon_app_dev/app/core/admin.py
+++ udemy/lon_app_dev/app/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.admin import Sample
+from core.models import Sample
 
 # Register your models here.
 
--- udemy/lon_app_dev/app/core/models.py
+++ udemy/lon_app_dev/app/core/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 # Test model to run a docker container
 
-class Sample(model.Models):
+class Sample(models.Model):
     attachment = models.FileField()
