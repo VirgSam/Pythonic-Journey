f3b8ca1cb9f80386915480f33493d20c1333d8f8
chris01 <chris_odum@yahoo.com>
2023-07-25 04:50:57 +0200

    Update settings.py DATABASES variable for Postgresql functionality

--- udemy/lon_app_dev/app/app/settings.py
+++ udemy/lon_app_dev/app/app/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST':os.environ.get('DB_HOST'),
         'NAME':os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
     }
 }
 
