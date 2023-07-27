899f52fd7e9854ac1effb1299d8ff238070d08de
chris01 <chris_odum@yahoo.com>
2023-07-27 15:20:33 +0200

    Update urls.py to create path access for static files

--- udemy/lon_app_dev/app/app/urls.py
+++ udemy/lon_app_dev/app/app/urls.py
@@ -23,3 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
