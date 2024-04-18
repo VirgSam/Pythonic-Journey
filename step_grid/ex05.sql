deeb01839c7dacfdefba7be85d2ee5b2f0f06770
VirgSam <chris_odum@yahoo.com>
2024-04-19 00:01:02 +0200

    insert into cities

---
+++ step_grid/ex05.sql
@@ -0,0 +1,6 @@
+INSERT INTO cities (name,country,population,area)
+VALUES ('Tokyo','Japan',38505000,8223);
+INSERT INTO cities (name,country,population,area)
+VALUES ('Delhi','india',28125000,2240),
+VALUES ('Shanghai','China',22125000,4015),
+VALUES ('Sao Paulp','Brazil',20935000,3043);
