648edf92618493302e5afe3d308904cec9eb48a4
VirgSam <chris_odum@yahoo.com>
2024-04-19 17:39:22 +0200

    Concatenation using concat function

--- step_grid/ex12.sql
+++ step_grid/ex12.sql
@@ -1,5 +1,8 @@
 -- String operators and functions
--- Concatenation
+-- Concatenation using pipe
 SELECT name || ',' || country FROM cities;
 
 SELECT name || ',' || country AS location FROM cities;
+
+-- Concatenation using concat function
+SELECT CONCAT(name, ', ', country) AS location FROM cities;
