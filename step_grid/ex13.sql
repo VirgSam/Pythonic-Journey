b1ebfc85b941eef46e8c594dee99158abd02e68e
VirgSam <chris_odum@yahoo.com>
2024-04-22 17:45:38 +0200

    Using where statement with BETWEEN operator

--- step_grid/ex13.sql
+++ step_grid/ex13.sql
@@ -1,16 +1,23 @@
 -- Using the Where statement
 SELECT name, area FROM cities WHERE area > 4000;
 
--- Using where statement with = operators
+-- Using where statement with = operator
 SELECT 
 name, area 
 FROM 
 cities 
 WHERE area = 8223;
 
--- Using where statement with != operators
+-- Using where statement with != operator
 SELECT 
 name, area 
 FROM 
 cities 
 WHERE area != 8223;
+
+-- Using where statement with BETWEEN operator
+SELECT 
+name, area 
+FROM 
+cities 
+WHERE area BETWEEN 2000 AND 4000;
