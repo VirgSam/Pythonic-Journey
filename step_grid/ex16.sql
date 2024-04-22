cd61baf63eae41ec878fc8f77cc86d16881b13e2
VirgSam <chris_odum@yahoo.com>
2024-04-22 21:50:01 +0200

    Using complex where exercise overview

---
+++ step_grid/ex16.sql
@@ -0,0 +1,8 @@
+/*Practicing where statements
+ Write a query that will print the name and price of all phones that sold greater than 500o units*/
+
+SELECT name, price
+FROM
+phones
+WHERE
+units_sold > 5000;
