f7bc8c5d6db0060639ceb642046b28e63575e7e3
VirgSam <chris_odum@yahoo.com>
2024-04-22 17:42:55 +0200

    using where statement and other operators 2

--- step_grid/ex13.sql
+++ step_grid/ex13.sql
@@ -1,9 +1,16 @@
 -- Using the Where statement
 SELECT name, area FROM cities WHERE area > 4000;
 
--- Using where statement with operators
+-- Using where statement with = operators
 SELECT 
 name, area 
 FROM 
 cities 
 WHERE area = 8223;
+
+-- Using where statement with != operators
+SELECT 
+name, area 
+FROM 
+cities 
+WHERE area != 8223;
