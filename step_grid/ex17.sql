cecf26cafc834deb3e0c3a7591607cd939346974
VirgSam <chris_odum@yahoo.com>
2024-04-23 06:30:45 +0200

    Use the DELETE STATEMENT

--- step_grid/ex17.sql
+++ step_grid/ex17.sql
@@ -5,3 +5,14 @@ SET
 population = 39505000
 WHERE
 name = 'Tokyo';
+
+/*Using the DELETE Statement*/
+DELETE
+FROM
+cities
+WHERE
+name = 'Tokyo';
+
+INSERT INTO cities(name,country,population,area)
+VALUES
+('Tokyo','Japan',39505000,8223);
