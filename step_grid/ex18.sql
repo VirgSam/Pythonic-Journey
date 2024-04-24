82a59c748e9d2e51652fb24e226259a30280e8e1
VirgSam <chris_odum@yahoo.com>
2024-04-24 06:12:28 +0200

    Insert multiple photos into photos table

--- step_grid/ex18.sql
+++ step_grid/ex18.sql
@@ -23,3 +23,13 @@ CREATE TABLE photos(
 INSERT INTO photos(url,user_id)
 VALUES
 ('http://one.jpg',4);
+
+-- Insert multiple photos into table
+INSERT INTO photos(url,user_id)
+VALUES
+('http://two.jpg',1),
+('http://25.jpg',1),
+('http://36.jpg',1),
+('http://754.jpg',2),
+('http://35.jpg',3),
+('http://256.jpg',4);
