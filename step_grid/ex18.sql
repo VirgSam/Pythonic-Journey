8d8e8beec86b38acb710c9c64502b3d9aa5ec674
VirgSam <chris_odum@yahoo.com>
2024-04-24 05:56:52 +0200

    Insert Users Table

--- step_grid/ex18.sql
+++ step_grid/ex18.sql
@@ -3,3 +3,11 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50)
 );
+
+-- Insert users into table
+INSERT INTO users(username)
+VALUES
+('monahan93'),
+('pferrer'),
+('si93onis'),
+('99stroman');
