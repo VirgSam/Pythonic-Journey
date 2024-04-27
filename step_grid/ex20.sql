051aedf6df138823044b9dc3184d018f9e27fa3c
VirgSam <chris_odum@yahoo.com>
2024-04-27 21:42:48 +0200

    Practice For Grouping and Aggregating

--- step_grid/ex20.sql
+++ step_grid/ex20.sql
@@ -85,3 +85,45 @@ SELECT title, name, ratings
 FROM reviews
 JOIN books ON books.id = reviews.book_id
 JOIN authors ON authors.id = books.author_id and authors.id = reviews.reviewer_id;
+
+/*Practice For Grouping and Aggregating
+Write a query that will print an author's id and the number of books they have authored.
+*/
+DROP TABLE reviews;
+DROP TABLE books;
+DROP TABLE authors;
+
+-- Create table authors
+CREATE TABLE authors(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+-- Create table books
+CREATE TABLE books(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    author_id INTEGER REFERENCES authors(id) ON DELETE CASCADE
+);
+
+-- Insert into authors table
+INSERT INTO authors(name)
+VALUES
+('JK Rowling'),
+('Stephen King'),
+('Agatha Christie'),
+('Dr Seuss');
+
+-- Insert into books table
+INSERT INTO books(title,author_id)
+VALUES
+('Chamber of Secrets',1),
+('Prisoner of Azkaban',2),
+('The Dark Tower',2),
+('Murder At the Links',3),
+('Affair At Stylest',3),
+('Cat in the Hat',3);
+
+SELECT author_id, COUNT(*)
+FROM books
+GROUP BY author_id;
