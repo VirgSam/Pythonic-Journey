f770a4c6df4c47517c9209bb45f68d67e85e0cba
VirgSam <chris_odum@yahoo.com>
2024-04-27 14:09:46 +0200

    Using group by function

--- step_grid/ex18.sql
+++ step_grid/ex18.sql
@@ -308,3 +308,8 @@ SELECT url,contents, username
 FROM comments
 JOIN photos ON photos.id = comments.photo_id
 JOIN users ON users.id = comments.user_id AND users.id = photos.user_id;
+
+-- Using group by function
+SELECT user_id
+FROM comments
+GROUP BY user_id;
