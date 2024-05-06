4e66227bbf325dac26d26b076d9573363c0684bb
VirgSam <chris_odum@yahoo.com>
2024-05-06 06:52:02 +0200

    Review of Join statent practice exercise

--- step_grid/ex21.sql
+++ step_grid/ex21.sql
@@ -734,3 +734,11 @@ VALUES
     SELECT paid, COUNT(*)
     FROM orders
     GROUP BY paid;
+
+	/*Join together the users and orders tables.  
+	Print the first_name and last_name of each user, 
+	then whether or not they have paid for their order.*/
+
+	SELECT first_name, last_name, paid
+	FROM users
+	JOIN orders ON orders.user_id = users.id;
