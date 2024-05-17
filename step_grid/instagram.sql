a25d008ff0652d122f384a18c42a251c94877efa
VirgSam <chris_odum@yahoo.com>
2024-05-17 11:03:03 +0200

    Practice exercise on recursive CTE's on instagram followers

--- step_grid/instagram.sql
+++ step_grid/instagram.sql
@@ -238,9 +238,27 @@ WHERE tags.created_at < '2010-01-07';
 
 -- Using recursive Common Table expressions
 WITH RECURSIVE countdown(val) AS (
-	SELECT  5 AS val
+	SELECT  5 AS val -- intial, Non-recursive query
 	UNION
-	SELECT val -1 FROM countdown WHERE val>1
+	SELECT val -1 FROM countdown WHERE val>1 -- Recursive query
 )
 SELECT *
 FROM countdown;
+
+-- Practice exercise on recursive CTE's on instagram followers
+	WITH RECURSIVE suggestions(leader_id,follower_id,depth) AS (
+			SELECT leader_id,follower_id, 1 AS depth
+			FROM followers
+			WHERE follower_id = 1000
+		UNION
+			SELECT followers.id, followers.follower_id, depth + 1
+			FROM followers
+			JOIN suggestions ON suggestions.leader_id = followers.follower_id
+			WHERE depth < 3
+
+	)
+	SELECT DISTINCT users.id, users.username
+	FROM suggestions
+	JOIN users ON users.id = suggestions.leader_id
+	WHERE depth > 1
+	LIMIT 30;
