6138e7f50ece97a3d97e00e72ce9046c4d30a288
VirgSam <chris_odum@yahoo.com>
2024-05-18 20:48:55 +0200

    Create a view for 10 recent posts 4

--- step_grid/instagram.sql
+++ step_grid/instagram.sql
@@ -309,3 +309,11 @@ JOIN users on users.id= recent_posts.user_id;
 SELECT username 
 FROM recent_posts
 JOIN users on users.id= recent_posts.user_id;
+
+-- How to delete or change a view
+CREATE OR REPLACE VIEW recent_posts AS (
+	SELECT *
+	FROM posts
+	ORDER BY created_at DESC
+	LIMIT 5
+);
