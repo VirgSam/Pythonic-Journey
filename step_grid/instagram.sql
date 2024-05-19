d6afc47717dcc232b1233e6bff06b9df8328c46d
VirgSam <chris_odum@yahoo.com>
2024-05-19 15:46:40 +0200

    Create a materialized view based on a weekly IG like system 3

--- step_grid/instagram.sql
+++ step_grid/instagram.sql
@@ -338,4 +338,19 @@ LEFT JOIN comments ON comments.id = likes.comment_id
 GROUP BY week
 ORDER BY week;
 
--- part 3
+-- part 3 to avoid wastage of resouces use materialized view
+CREATE MATERIALIZED VIEW weekly_likes AS (
+	SELECT 
+	date_trunc('week',COALESCE(posts.created_at, comments.created_at)) AS week,
+	COUNT(posts.id) AS num_likes_for_posts,
+	COUNT(comments.id) AS num_likes_for_comments
+	FROM likes
+	LEFT JOIN posts ON posts.id = likes.post_id
+	LEFT JOIN comments ON comments.id = likes.comment_id
+	GROUP BY week
+	ORDER BY week
+) WITH DATA;
+
+--- to access Materialized view
+SELECT * 
+FROM weekly_likes;
